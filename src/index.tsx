2a9c294334b1c79986cd61ae610187fb91026274
Labici Ivan <ioanlabici@gmail.com>
2025-05-14 10:27:44 +0300

    FB-1: Pictures and small css changes.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Router from './Router.tsx'
+import { AuthenticationProvider } from './context/AuthenticationContext.tsx'
+import AppointmentUpdateProvider from './context/AppointmentUpdateContex.tsx'
+import ErrorProvider from './context/ErrorContext.tsx'
+import CalendarSelectedDayProvider from './context/CalendarSelectedDayContext.tsx'
+import SettingsProvider from './context/SettingsContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthenticationProvider>
+      <AppointmentUpdateProvider>
+        <ErrorProvider>
+          <CalendarSelectedDayProvider>
+            <SettingsProvider>
+              <Router />
+            </SettingsProvider>
+          </CalendarSelectedDayProvider>
+        </ErrorProvider>
+      </AppointmentUpdateProvider>
+    </AuthenticationProvider>
   </StrictMode>,
 )
